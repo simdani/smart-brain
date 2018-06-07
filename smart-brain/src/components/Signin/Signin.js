995fa6620c65f199ee00c563d56d9e457979876e
simdani <dranxman@gmail.com>
2018-06-07 22:37:49 +0300

    add register example and update home page with user data

--- smart-brain/src/components/Signin/Signin.js
+++ smart-brain/src/components/Signin/Signin.js
@@ -29,8 +29,9 @@ class Signin extends React.Component {
       })
     })
       .then(response => response.json())
-      .then(data => {
-        if (data === 'success') {
+      .then(user => {
+        if (user) {
+          this.props.loadUser(user);
           this.props.onRouteChange('home');
         }
       });
