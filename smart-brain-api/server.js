d822f5115e6976bae555313f6acacad16a768ee4
simdani <dranxman@gmail.com>
2018-06-10 18:35:21 +0300

    add user profile get route

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -68,18 +68,20 @@ app.post('/signin', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const {id} = req.params;
-  let found = false;
 
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-
-  if (!found) {
-    res.status(400).json('user not found');
-  }
+  db.select('*')
+    .from('users')
+    .where({
+      id: id
+    })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('user not found')
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'));
 });
 
 app.put('/image', (req, res) => {
