842d3eaeaa618965aa8bf88a621fa458eae5a519
simdani <dranxman@gmail.com>
2018-06-18 19:24:11 +0300

    refactor server.js into controllers and fix react initial state

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -4,6 +4,12 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+// load controllers
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const image = require('./controllers/image');
+const profile = require('./controllers/profile');
+
 // connect to database
 const db = knex({
   client: 'pg',
@@ -21,87 +27,17 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.json(database.users);
+  res.json('ok');
 });
 
 // make sign in request
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            res.json(user[0]);
-          });
-      } else {
-        res.status(400).json('wrong credentials');
-      }
-    })
-    .catch(err => res.status(400).json('wrong credentials'));
-});
-
-app.get('/profile/:id', (req, res) => {
-  const {id} = req.params;
-
-  db.select('*')
-    .from('users')
-    .where({
-      id: id
-    })
-    .then(user => {
-      if (user.length) {
-        res.json(user[0])
-      } else {
-        res.status(400).json('user not found')
-      }
-    })
-    .catch(err => res.status(400).json('error getting user'));
-});
-
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
+// get user pfofile
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
 // update user image entries
-app.put('/image', (req, res) => {
-  const {id} = req.body;
-
-  db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => res.json(entries[0]))
-    .catch(err => res.status(400).json('unable to get entries'));
-});
-
+app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 // register new user
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-
-  const hash = bcrypt.hashSync(password);
-
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-        .returning('*')
-        .insert({
-          email: loginEmail[0],
-          name: name,
-          joined: new Date()
-        })
-        .then(user => {
-          res.json(user[0]);
-        });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('Unable to register.'));
-});
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
 const port = 5000;
 app.listen(port, () => {
--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -28,23 +28,25 @@ const particlesOptions = {
   }
 };
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+};
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -97,6 +99,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, {entries: count}))
             })
+            .catch(err => console.log(err));
         }
         this.displayFacebox(this.calculateFaceLocation(response))
       })
@@ -105,7 +108,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     }
     else if (route === 'home') {
       this.setState({isSignedIn: true })
